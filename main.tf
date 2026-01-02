215cb981feb46066e4c2beb56914b60614ec7686
Suhani Joshi <suhanijoshi@suhanis-air.lan>
2026-01-02 09:41:03 -0600

    added security group

--- main.tf
+++ main.tf
@@ -5,3 +5,22 @@ resource "aws_instance" "exampleEC2" {
 
 
 }
+resource "aws_security_group" "ssh_sg" {
+  name = "ssh-access"
+  description = "Allow SSH inbound"
+  
+  ingress{
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = [ "47.186.55.11/32"]
+
+  }
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+}
