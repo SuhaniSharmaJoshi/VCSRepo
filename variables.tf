6c0dc6bb278fc11320b44c3ccccd047732d97387
Suhani Joshi <suhanijoshi@suhanis-air.lan>
2026-01-02 07:13:19 -0600

    instance type changed

--- variables.tf
+++ variables.tf
@@ -6,7 +6,7 @@ variable "aws_region" {
 variable "instance_type" {
   description = "The EC2 instance's type."
   type        = string
-  default     = "t2.micro"
+  default     = "t3.micro"
 }
 variable "key_name" {
 
