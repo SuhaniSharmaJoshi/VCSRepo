8242308a01d6a6364a1384aa36998ee8dffd4972
Suhani Joshi <suhanijoshi@suhanis-air.lan>
2026-01-02 06:26:15 -0600

    Add terraform configuration to create EC2 instance

---
+++ main.tf
@@ -0,0 +1,9 @@
+resource "aws_instance" "exampleEC2" {
+  ami           = "ami-0c02fb55956c7d316" # Amazon Linux 2 (us-east-1)
+  instance_type = var.instance_type
+  key_name      = var.key_name
+
+  tags = {
+    Name = var.instance_name
+  }
+}
