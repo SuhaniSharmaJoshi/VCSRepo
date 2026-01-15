27e0c60a1ca1654982404fcf571cd8f0a5d73353
Suhani Joshi <suhanijoshi@suhanis-air.lan>
2026-01-15 12:30:55 -0600

    checking if my sentinel works

--- outputs.tf
+++ outputs.tf
@@ -4,6 +4,6 @@ output "public_ip" {
 output "instance_ami" {
  value = aws_instance.exampleEC2.ami
 }
-output "example_eip" {
-   value = aws_eip.example_eip.public_ip  
-}
+#output "example_eip" {
+  # value = aws_eip.example_eip.public_ip  
+#}
