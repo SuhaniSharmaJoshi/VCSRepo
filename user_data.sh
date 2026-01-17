f9eb69d28194678fbd31246e6e1731848cfe9c7d
Suhani Joshi <suhanijoshi@suhanis-air.lan>
2026-01-16 18:46:14 -0600

    lets check if it works now

--- user_data.sh
+++ user_data.sh
@@ -5,4 +5,4 @@ yum install -y httpd
 systemctl start httpd
 systemctl enable httpd
 
-echo "Hello from terraform user_data on $(hostname)" > /var/www/html/index.html
+echo "Hello from Terraform user_data on $(hostname)" > /var/www/html/index.html
